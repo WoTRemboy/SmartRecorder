5232653c96be6920a3d132bee6a61b7b46cbd058
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-10-08 19:57:20 +0300

    Initial Commit

---
+++ SmartRecorder/SmartRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartRecorderApp.swift
+//  SmartRecorder
+//
+//  Created by Roman Tverdokhleb on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
