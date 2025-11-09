0c7455c96b296bca67861f6aef4a27dfcba061d6
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-11-09 15:18:54 +0200

    feat: add tabbar router

---
+++ SmartRecorder/NotesView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesView.swift
+//  SmartRecorder
+//
+//  Created by Roman Tverdokhleb on 09/11/2025.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text("NotesView")
+    }
+}
+
+#Preview {
+    NotesView()
+}
