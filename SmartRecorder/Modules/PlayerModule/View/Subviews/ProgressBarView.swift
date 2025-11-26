4742bb123818f76159a8e4d1bd4f6e3549d245c3
Victoria Miroshnik <victoriafreeman990@gmail.com>
2025-11-26 15:12:05 +0300

    feat: add slider to player

--- SmartRecorder/Modules/PlayerModule/View/Subviews/ProgressBarView.swift
+++ SmartRecorder/Modules/PlayerModule/View/Subviews/ProgressBarView.swift
@@ -12,14 +12,35 @@ struct ProgressBarView: View {
     @State private var viewModel = PlayerViewModel()
     @State private var sliderProgress: CGFloat = 0.0
     @State private var totalWidth: CGFloat = 0.0
+    @State private var sliderChangeInProgress: Bool = false
+    @State private var currentTimeLabel: TimeInterval = 0.0
+    
+    private let sliderHeight = 7.0
     
     var body: some View {
         VStack {
-            ZStack(alignment: .leading) {
+            ZStack(alignment: .center) {
                 Capsule()
                     .foregroundColor(.SupportColors.lightBlue)
                     .frame(width: sliderProgress)
+                    .position(x: sliderProgress / 2.0, y: sliderHeight / 2.0)
+                    .animation(.interactiveSpring, value: sliderProgress)
                 
+                Circle()
+                    .foregroundColor(.SupportColors.lightBlue)
+                    .opacity(0.0)
+                    .contentShape(Circle())
+                    .frame(width: 15.0, height: 15.0)
+                    .gesture(
+                        DragGesture(minimumDistance: 7.5, coordinateSpace: .named("slider"))
+                            .onChanged { a in
+                                onSliderChanged(a.location.x)
+                            }
+                            .onEnded { a in
+                                onSliderChangeEnded(a.location.x)
+                            }
+                    )
+                    .position(x: sliderProgress, y: sliderHeight / 2.0)
                 GeometryReader { geometry in
                     Capsule().foregroundColor(.SupportColors.lightBlue).opacity(0.29)
                         .onAppear {
@@ -28,48 +49,57 @@ struct ProgressBarView: View {
                 }
                 
             }
-            .frame(height: 7)
+            .frame(height: sliderHeight)
             .padding(.bottom, 13)
+            .coordinateSpace(name: "slider")
             
             HStack {
-                Text(String("\(timeIntervalToString(viewModel.currentTime))"))
+                Text(String("\(timeIntervalToString(currentTimeLabel))"))
                 Spacer()
                 Text(String("\(timeIntervalToString(viewModel.duration))"))
             }
             .font(Font.caption(.semibold))
             .foregroundColor(.SupportColors.blue.opacity(0.75))
             
-            HStack(spacing: 20) {
-                Button {
-                    viewModel.currentTime -= 10.0
+            GlassEffectContainer(spacing: 10) {
+                HStack(spacing: 10) {
+                    Button {
+                        viewModel.currentTime -= 10.0
+                    }
+                    label: {
+                        ZStack {
+                            Circle().frame(width: 60.0, height: 60.0).foregroundStyle(Color(.clear))
+                            Image(systemName: "backward.fill")
+                                .font(.system(size: 20, weight: .regular))
+                        }
+                    }
+                    .glassEffect(.regular.interactive())
+                    
+                    Button {
+                        viewModel.toggle()
+                    }
+                    label: {
+                        ZStack {
+                            Circle().frame(width: 100, height: 100).foregroundStyle(Color(.clear))
+                            Image(systemName: viewModel.isPlaying ? "pause.fill" : "play.fill")
+                                .font(.system(size: 40, weight: .regular))
+                            
+                        }
+                    }
+                    .glassEffect(.regular.interactive())
+                    
+                    Button {
+                        viewModel.currentTime += 10.0
+                    }
+                    label: {
+                        ZStack {
+                            Circle().frame(width: 60.0, height: 60.0).foregroundStyle(Color(.clear))
+                            Image(systemName: "forward.fill")
+                                .font(.system(size: 20, weight: .regular))
+                        }
+                    }
+                    .glassEffect(.regular.interactive())
                 }
-                label: {
-                    Image(systemName: "backward.fill")
-                        .font(.system(size: 20, weight: .regular))
-                        .padding(20)
-                }
-                .glassEffect(.regular.interactive(), in: .circle)
-                
-                // нужно исправить поведение кнопки по окончании аудио
-                Button {
-                    viewModel.toggle()
-                }
-                label: {
-                    Image(systemName: viewModel.isPlaying ? "pause.fill" : "play.fill")
-                        .font(.system(size: 40, weight: .regular))
-                        .padding(30)
-                }
-                .glassEffect(.regular.interactive(), in: .circle)
-                
-                Button {
-                    viewModel.currentTime += 10.0
-                }
-                label: {
-                    Image(systemName: "forward.fill")
-                        .font(.system(size: 20, weight: .regular))
-                        .padding(20)
-                }
-                .glassEffect(.regular.interactive(), in: .circle)
             }
             .foregroundColor(.SupportColors.blue)
             .padding(.all)
@@ -82,7 +112,22 @@ struct ProgressBarView: View {
     }
     
     private func calcSliderProgress() {
-        sliderProgress = totalWidth / CGFloat(viewModel.duration) * CGFloat(viewModel.currentTime)
+        if !sliderChangeInProgress {
+            sliderProgress = totalWidth * (CGFloat(viewModel.currentTime) / CGFloat(viewModel.duration))
+            currentTimeLabel = viewModel.currentTime
+        }
+    }
+    
+    private func onSliderChanged(_ value: CGFloat) {
+        sliderProgress = min(totalWidth, max(value, 0.0))
+        sliderChangeInProgress = true
+        currentTimeLabel = CGFloat(viewModel.duration) * (sliderProgress / totalWidth)
+    }
+    
+    private func onSliderChangeEnded(_ value: CGFloat) {
+        sliderChangeInProgress = false
+        viewModel.currentTime = CGFloat(viewModel.duration) * (sliderProgress / totalWidth)
+        currentTimeLabel = viewModel.currentTime
     }
 }
 
@@ -92,7 +137,7 @@ private func timeIntervalToString(_ interval: TimeInterval) -> String {
     let seconds = totalSeconds % 60
     let minutes = totalMinutes % 60
     let hours = totalMinutes / 60
-
+    
     return String(format: "%d:%02d:%02d", hours, minutes, seconds)
     
 }
@@ -100,5 +145,5 @@ private func timeIntervalToString(_ interval: TimeInterval) -> String {
 #Preview {
     ProgressBarView()
         .background(Color.BackgroundColors.primary
-        .ignoresSafeArea(.all))
+            .ignoresSafeArea(.all))
 }
